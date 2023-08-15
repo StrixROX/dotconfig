23c2080cda2a09533ef2558c75494ffe1cf7acc1
StrixROX <40167899+StrixROX@users.noreply.github.com>
2023-08-15 16:24:00 +0530

    Created wrapping functions and main menu. Added --all flag

--- setup.sh
+++ setup.sh
@@ -9,40 +9,69 @@ if [ "${PWD##*/}" != "dotconfig" ]; then
   exit
 fi
 
-if [ -f ./.bash_aliases ]; then
-  echo -e "${YELLOW}.bash_aliases${RESET}"
-  if [ ! -f ~/.bash_aliases ]; then
-    echo -e "  Copying .bash_aliases > ~/.bash_aliases"
-    cat ./.bash_aliases > ~/.bash_aliases
-  else
-    read -p "  ~/.bash_aliases file already exists, [o]verwrite/[a]ppend/[S]kip? " action
-    if [[ $action == [oO] ]]; then
+function copyBashAliases {
+  if [ -f ./.bash_aliases ]; then
+    echo -e "${YELLOW}.bash_aliases${RESET}"
+    if [ ! -f ~/.bash_aliases ]; then
+      echo -e "  Copying .bash_aliases > ~/.bash_aliases"
       cat ./.bash_aliases > ~/.bash_aliases
-    elif [[ $action == [aA] ]]; then
-      cat ./.bash_aliases >> ~/.bash_aliases
     else
-      echo "  Skipping..."
+      read -p "  ~/.bash_aliases already exists, [o]verwrite/[a]ppend/[S]kip? " action
+      if [[ $action == [oO] ]]; then
+        cat ./.bash_aliases > ~/.bash_aliases
+      elif [[ $action == [aA] ]]; then
+        cat ./.bash_aliases >> ~/.bash_aliases
+      else
+        echo "  Skipping..."
+      fi
     fi
+    echo -e "  ${GREEN}done${RESET}"
   fi
-  echo -e "  ${GREEN}done${RESET}"
+}
+
+function copyConfigs {
+  if [ -f ./kitty/*.conf ]; then
+   if [ ! -d ~/.config/kitty ]; then
+      echo "Creating ~/.config/kitty folder..."
+      mkdir -p ~/.config/kitty
+    fi
+
+    for f in kitty/*.conf
+    do
+      echo -e "${YELLOW}${f}${RESET}"
+      if [ ! -f ~/.config/$f ]; then
+        echo -e "  Copying $f > ~/.config/$f"
+        cat $f > ~/.config/$f
+      else
+        read -p "  ~/.config/$f already exists, [o]verwrite/[a]ppend/[S]kip? " action
+        if [[ $action == [oO] ]]; then
+          cat $f > ~/.config/$f
+        elif [[ $action == [aA] ]]; then
+          cat $f >> ~/.config/$f
+        else
+          echo "  Skipping..."
+        fi
+      fi
+      echo -e "  ${GREEN}done${RESET}"
+    done
+  fi
+}
+
+if [[ $1 && $1 == "--all" ]]; then
+  copyBashAliases
+  copyConfigs
+else
+  read -p "Copy .bash_aliases?(y/N) " c1
+  if [[ $c1 == [yY] ]]; then
+    copyBashAliases
+  fi
+
+  read -p "Copy configuration files?(y/N) " c2
+  if [[ $c2 == [yY] ]]; then
+    copyConfigs
+  fi
 fi
 
-if [ -f ./kitty/*.conf ]; then
-  if [ ! -d ~/.config/kitty ]; then
-    echo "Creating ~/.config/kitty folder..."
-    mkdir -p ~/.config/kitty
-  fi
-
-  for f in kitty/*.conf
-  do
-    echo -e "${YELLOW}${f}${RESET}"
-    if [ ! -f ~/.config/$f ]; then
-      echo -e "  Copying $f > ~/.config/$f"
-      cat $f > ~/.config/$f
-    fi
-    echo -e "    ${GREEN}done${RESET}"
-  done
-  echo -e "  ${GREEN}done${RESET}"
+if [[ $c1 || $c2 ]]; then
+  echo -e "\n${BOLD}${GREEN}All set!${RESET}"
 fi
-
-echo -e "${BOLD}${GREEN}Script finished. Enjoy!${RESET}"
